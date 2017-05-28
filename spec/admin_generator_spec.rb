3c43cf61ebc0b6789765cae5510c04e098113ad9
jiikko <n905i.1214@gmail.com>
2017-05-28 14:19:39 +0900

    Fix spec

--- spec/admin_generator_spec.rb
+++ spec/admin_generator_spec.rb
@@ -4,8 +4,4 @@ describe AdminGenerator do
   it 'has a version number' do
     expect(AdminGenerator::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
